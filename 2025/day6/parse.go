1169822a1abc9ac79d72f37b6dda7d0beaf47854
Scott Doxey <neogeek0101@gmail.com>
2025-12-07 15:01:55 -0500

    Added day 6 part 2.

--- 2025/day6/parse.go
+++ 2025/day6/parse.go
@@ -4,6 +4,7 @@ import (
 	"advent-of-code-2025/utils"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -38,9 +39,39 @@ func part1(lines []string) int64 {
 	return result
 }
 
-func part2(lines []string) int {
-	var result = 0
+func part2(lines []string) int64 {
+	var result int64 = 0
 
+	data, operators := parseDataFromInput(lines)
+
+	for rowIndex, row := range data {
+		operator := strings.TrimSpace(operators[rowIndex])
+
+		var internalDataRow [][]string
+
+		for _, col := range row {
+			internalDataRow = append(internalDataRow, strings.Split(col, ""))
+		}
+
+		var values []int64
+
+		for _, number := range utils.SwapColumnsAndRows(internalDataRow) {
+			parsedNumber, _ := strconv.ParseInt(strings.TrimSpace(strings.Join(number, "")), 10, 64)
+
+			values = append(values, parsedNumber)
+		}
+
+		utils.ReverseInt64(values)
+
+		switch operator {
+		case "+":
+			result += utils.SumArray(values)
+		case "*":
+			result += utils.MultiplyArray(values)
+		default:
+			fmt.Println("unknown operator")
+		}
+	}
 	return result
 }
 
